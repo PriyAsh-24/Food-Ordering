29f6e38f752cf37696ca934ddcc8e83a3384fc5b
Priyash Sharma <146582441+PriyAsh-24@users.noreply.github.com>
2024-04-07 20:42:25 +0530

    Updates

---
+++ Grocery.a391de98.js
@@ -0,0 +1,2 @@
+var e;(0,(e=globalThis.parcelRequire072b).register)("8V5xr",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var o=e("1IdZj"),t=()=>(0,o.jsx)("h1",{children:"This is Our Grocery Page with many Child Components"})});
+//# sourceMappingURL=Grocery.a391de98.js.map
